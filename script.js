e4fd9300aa9f118d60a4935327dff41d7a1ec061
Christian Franco <132075071+c-franco@users.noreply.github.com>
2024-05-25 11:06:26 +0100

    Work experience page

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 let menuVisible = false;
 
-const navScroll = document.querySelector("#header");
-const navMenuScroll1 = document.querySelector(".li-menu1");
-const navMenuScroll2 = document.querySelector(".li-menu2");
-const navMenuScroll3 = document.querySelector(".li-menu3");
-const navMenuScroll4 = document.querySelector(".li-menu4");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 200) {
-    navScroll.classList.add("nav-scrolled");
-    navMenuScroll1.classList.add("nav-responsive-scrolled");
-    navMenuScroll2.classList.add("nav-responsive-scrolled");
-    navMenuScroll3.classList.add("nav-responsive-scrolled");
-    navMenuScroll4.classList.add("nav-responsive-scrolled");
-  } else if (window.scrollY <= 200) {
-    navScroll.classList.remove("nav-scrolled");
-    navMenuScroll1.classList.remove("nav-responsive-scrolled");
-    navMenuScroll2.classList.remove("nav-responsive-scrolled");
-    navMenuScroll3.classList.remove("nav-responsive-scrolled");
-    navMenuScroll4.classList.remove("nav-responsive-scrolled");
-  }
-});
-
 function showHideMenu() {
   if (menuVisible) {
     document.getElementById("nav").classList = "";
